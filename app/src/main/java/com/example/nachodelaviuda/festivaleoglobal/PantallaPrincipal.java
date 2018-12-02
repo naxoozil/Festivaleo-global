89d2ed091cc47478c506a9860cf72b713d78a0ec
naxoozil <nachoviuda@gmail.com>
2018-12-02 12:37:49 +0100

    version final de entrega

--- app/src/main/java/com/example/nachodelaviuda/festivaleoglobal/PantallaPrincipal.java
+++ app/src/main/java/com/example/nachodelaviuda/festivaleoglobal/PantallaPrincipal.java
@@ -1,5 +1,7 @@
+
 package com.example.nachodelaviuda.festivaleoglobal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,13 +9,54 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class PantallaPrincipal extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_principal,container,false);
+        View vista = inflater.inflate(R.layout.fragment_principal,container,false);
+        Button butEur = (Button) vista.findViewById(R.id.butEuropa);
+        Button butNor = (Button) vista.findViewById(R.id.butNorAm);
+        Button butSur = (Button) vista.findViewById(R.id.butSurAm);
+        Button butAsia = (Button) vista.findViewById(R.id.butAsia);
+
+        butEur.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Utilidades.proveniencia = "europa";
+                Intent intentoEuropa = new Intent(getContext(), ListaDeFestivales.class);
+                startActivity(intentoEuropa);
+            }
+        });
+        butNor.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Utilidades.proveniencia = "norteamerica";
+                Intent intentoAmNor = new Intent(getContext(), ListaDeFestivales.class);
+                startActivity(intentoAmNor);
+            }
+        });
+        butSur.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Utilidades.proveniencia = "latinoamerica";
+                Intent intentoAmericaSur = new Intent(getContext(), ListaDeFestivales.class);
+                startActivity(intentoAmericaSur);
+            }
+        });
+        butAsia.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Utilidades.proveniencia = "asia";
+                Intent intentoAsia = new Intent(getContext(), ListaDeFestivales.class);
+                startActivity(intentoAsia);
+            }
+        });
+
+
+        return vista;
 
     }
 }
